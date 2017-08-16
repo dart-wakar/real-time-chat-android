d0b3f40275321f503b3d1efca0728d97daa99263
dart-wakar <wakar@whitelogic.in>
2017-08-16 23:52:14 +0530

    login activity basic setup

--- app/src/main/java/com/wakarkhan/realtimechatandroid/MainActivity.java
+++ app/src/main/java/com/wakarkhan/realtimechatandroid/MainActivity.java
@@ -2,12 +2,35 @@ package com.wakarkhan.realtimechatandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText etUsername;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"lol");
+        initViews();
+    }
+
+    private void initViews() {
+        etUsername = (EditText)findViewById(R.id.et_username);
+        btnLogin = (Button)findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener((View view) -> login());
+    }
+
+    private void login() {
+        String username = etUsername.getText().toString();
+        Log.d(TAG,username);
     }
 }
