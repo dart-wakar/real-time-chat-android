31334feea2249facb7ff30d80622dce9e2723be3
dart-wakar <wakar@whitelogic.in>
2017-08-17 00:34:30 +0530

    login event emition

--- app/src/main/java/com/wakarkhan/realtimechatandroid/MainActivity.java
+++ app/src/main/java/com/wakarkhan/realtimechatandroid/MainActivity.java
@@ -7,6 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -14,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText etUsername;
     private Button btnLogin;
 
+    private Socket socket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"lol");
         initViews();
+        initSocketIo();
     }
 
     private void initViews() {
@@ -32,5 +41,32 @@ public class MainActivity extends AppCompatActivity {
     private void login() {
         String username = etUsername.getText().toString();
         Log.d(TAG,username);
+        socket.emit("login",username);
+    }
+
+    private void initSocketIo() {
+        try {
+            socket = IO.socket("http://10.1.1.25:3000");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG,"connected");
+            }
+        });
+        socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG,"ERROR in connection");
+            }
+        });
+        socket.on(Socket.EVENT_CONNECT_TIMEOUT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG,"Timeout");
+            }
+        });
     }
 }
